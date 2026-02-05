b44a72dff06a26bbd74c247d9206fccdf62a9e7d
Lee Samuel <Lsamuel1962@gmail.com>
2026-02-04 21:51:23 -0800

    state exercise solution

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
 import WordCard from "./Components/WordCard/WordCard";
+import React from "react";
 
 function App() {
+  const [words, setWords] = React.useState([
+    { front: "translation", back: "Übersetzung" },
+    { front: "egg", back: "Ei" },
+    { front: "taxi", back: "Taxi" },
+    { front: "paper", back: "Papier" },
+    { front: "square", back: "Platz" },
+  ]);
+
   return (
-    <div className="app">
+    <div className="App">
       <header className="App-header">Dictionary App</header>
       <main className="cards-container">
-        <WordCard front="translation" back="Übersetzung" />
-        <WordCard front="egg" back="Ei" />
-        <WordCard front="taxi" back="Taxi" />
-        <WordCard front="paper" back="Papier" />
-        <WordCard front="square" back="Platz" />
+        {/* Mapping through your state array to render cards dynamically */}
+        {words.map((word, index) => (
+          <WordCard key={index} front={word.front} back={word.back} />
+        ))}
       </main>
     </div>
   );
