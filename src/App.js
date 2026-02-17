c949071d2dcdf7ee63a6436212f399d7ee7d9477
Lee Samuel <Lsamuel1962@gmail.com>
2026-02-17 01:29:49 -0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import {useState, useEffect} from "react";
-=======
-import React, { useEffect } from "react";
-
->>>>>>> Stashed changes
+import React, { useState, useEffect } from "react";
 import WordForm from "./Components/WordForm/WordForm";
 import CardsContainer from "./Components/CardsContainer/CardsContainer";
 
-
-// This function is a Statefull Component - it has state and logic to manage that state.
 function App() {
-<<<<<<< Updated upstream
   const [words, setWords] = useState([]);
 
-useEffect(
-  () => {
-    fetch('./constants/words.json')
-      .then(x => x.json())
-      .then(data => setWords(data));
-    console.log('USEEFFECT');
-    },
-    [] // dependency array
-);
-=======
-  const [words, setWords] = React.useState([]);
+  useEffect(() => {
+    fetch("./constants/words.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setWords(data);
+      })
+      .catch((err) => console.error("Error fetching words:", err));
+  }, []);
 
-  useEffect(
-    () => {
-      fetch("./constants/words.json")
-        .then((x) => x.json())
-        .then((data) => setWords(data));
-      console.log("App USEEFFECT");
-    },
-    [], // empty dependency array - this useEffect will only run once, when the component first mounts.
-  );
->>>>>>> Stashed changes
-
-  const addWord = (front, back) => {
-    const newWords = [...words, { front, back }];
-    setWords(newWords);
+  const addWord = (word) => {
+    setWords([...words, word]);
   };
 
-  const deleteWord = (front) => {
-    const newWords = words.filter((word) => word.front !== front); // This is a Boolean expression that returns true for all words that do NOT match the front of the word to delete.
-    setWords(newWords);
+  const deleteWord = (wordFront) => {
+    const updatedWords = words.filter((word) => word.front !== wordFront);
+    setWords(updatedWords);
   };
 
   return (
     <div className="App">
-      <header className="App-header">Dictionary App</header>
-      <main>
-        <WordForm addWord={addWord} />
-        <CardsContainer words={words} deleteWord={deleteWord} />
-      </main>
+      <WordForm addWord={addWord} />
+      <CardsContainer words={words} deleteWord={deleteWord} />
     </div>
   );
 }
